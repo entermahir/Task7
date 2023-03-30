1b0860c50c817ce10cb605c63c21e6fadae7d56b
entermahir <mmemmedxanov@yahoo.com>
2023-03-30 20:37:48 +0400

    task7.1

--- Program.cs
+++ Program.cs
@@ -1,10 +1,62 @@
-﻿namespace Task7
+﻿using System.Diagnostics.Metrics;
+
+namespace Task7
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+
+            Console.WriteLine(Ask7);
+
         }
+        static void Ask7()
+
+
+        {
+
+            //1) Verilmish metnde {a} simvolun sayi {b} simvolun sayinda nece defe coxdur?
+
+            string a = "1) Verilmish metnde {a} simvolun sayi {b} simvolun sayiVVnda nece defe coxdur?";
+            int Count2 = 0;
+            int Count1 = 0;
+            char chqr = 'a';
+            char chqr1 = 'b';
+
+
+            foreach (char chr in a)
+            {
+                if (chr == chqr)
+                {
+                    Count1++;
+
+
+
+
+                }
+
+                if (chr == chqr1)
+                {
+                    Count2++;
+                }
+
+            }
+
+            Console.WriteLine($" a simvolu---" + Count1);
+            Console.WriteLine($" b simvolu---" + Count2);
+
+
+            Console.WriteLine($"Cavab---" + Count1 / Count2);
+
+
+        }
+
+
+
     }
+
+
+
+
 }
