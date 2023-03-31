1ac51d26961d834f71aaeaab2901ba103adbdb76
entermahir <mmemmedxanov@yahoo.com>
2023-03-31 23:43:39 +0400

    7.5 done

--- Program.cs
+++ Program.cs
@@ -6,26 +6,28 @@ namespace Task7
     {
         static void Main(string[] args)
         {
-            //Task71
-            //Task72
-            // 3) Verilmish metnde sol terefden tek yerde dayanan simvollardan necesi { b}
-            // -ye beraberdir.
-            string h = "b)bbb";
-            char b = 'b';
-            int Counter = 0;
-            for (int i = 0; i < h.Length; i++)
+            //Task71 //done;
+            //Task72 //done;
+            //Task73 //done;
+            //Task74 //done;
+            //5) Verilmish metnde sol terefden saydiqda {a},{b},{c} simollarindan hansi birinci gelir?
+            string y = "5) Verilmish metnde sol terefden saydiqda {a},{b},{c} simollarindan hansi birinci gelir?";
+            char ind1 = 'a';
+            char ind2 = 'b';
+            char ind3 = 'c';
+            for (int i = 0; i < y.Length; i++)
             {
-                if (i%2==0)
+                if (y[i] == ind1)
                 {
-                    if (b == h[i])
-                    {
-                        Counter++;
+                    Console.WriteLine("{a} simvolu");break;
 
+                }else if (y[i] == ind2)
+                {
+                    Console.WriteLine("b simvolu");break;
 
-
-                    }
-
-
+                }else if (y[i] == ind3)
+                {
+                    Console.WriteLine("c simvolu");break;
 
                 }
 
@@ -35,8 +37,6 @@ namespace Task7
 
 
             }
-
-                        Console.WriteLine(Counter);
 
 
 
@@ -130,7 +130,27 @@ namespace Task7
 
         static void Task73(string a)
         {
+            // 3) Verilmish metnde sol terefden tek yerde dayanan simvollardan necesi { b}
+            // -ye beraberdir.
+            string h = "3) Verilmish metnde sol terefden tek yerde dayanan simvollardan necesi {b} -ye beraberdir.";
+            char b = 'b';
+            int Counter = 0;
+            for (int i = 0; i < h.Length; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    if (b == h[i])
+                    {
+                        Counter++;
 
+                    }
+
+                }
+
+
+            }
+
+            Console.WriteLine(Counter);
 
 
 
@@ -141,7 +161,37 @@ namespace Task7
 
         }
 
+        static void Task74(string a)
+        {
 
+            /*4) Verilmish metnde sol terefden ilk rast gelinen { a}
+            simvolunun yeri tek ededdi yoxsa cut ?*/
+
+            string q = "4) Verilmish metnde sol terefden ilk rast gelinen {a} simvolunun yeri tek ededdi yoxsa cut ?";
+            char indx = 'a';
+            for (int i = 0; i < q.Length; i++)
+            {
+                if (indx == q[i])
+                {
+                    if (i % 2 == 0)
+                    {
+                        Console.WriteLine("cut"); break;
+
+                    }
+                    else Console.WriteLine("tek"); break;
+
+                }
+
+
+            }
+
+
+
+
+
+
+
+        }
 
     }
 
